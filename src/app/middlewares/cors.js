07c1dee094d5c3e756943024dc46e47db53c170c
Thiago Cruz <thiagocruz.eu@gmail.com>
2025-03-18 17:37:15 -0300

    feat: implement multiple origins allowance for cors

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,7 +1,14 @@
 module.exports = (request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  response.setHeader('Access-Control-Allow-Methods', '*');
-  response.setHeader('Access-Control-Allow-Headers', '*');
-  response.setHeader('Access-Control-Max-Age', '10');
+  const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5173'];
+  const origin = request.header('Origin');
+  const isOriginAllowed = allowedOrigins.includes(origin);
+
+  if (isOriginAllowed) {
+    response.setHeader('Access-Control-Allow-Origin', origin);
+    response.setHeader('Access-Control-Allow-Methods', '*');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    response.setHeader('Access-Control-Max-Age', '10');
+  }
+
   next();
 };
